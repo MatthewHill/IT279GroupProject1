71018e32e5907582f3fdafb767977d185f3474ca
comrad121 <34254430+comrad121@users.noreply.github.com>
2018-02-12 17:16:15 -0600

    Needs Stack.cpp and Stack.h implementation.
    Also needs review and suggestion for 'U' and 'R' function implementation

--- StackProgram/Source.cpp
+++ StackProgram/Source.cpp
@@ -1,7 +1,9 @@
+/* This rendition should be modified after Stack.h and Stack.cpp are flushed out.  I have also not yet decided how to implement the Undo and Redo methods. - Matthew*/
 #include <iostream>
 #include <sstream>
 #include <string>
 #include "Stack.h"
+#include "Stack.cpp"
 using namespace std;
 int main()
 {
@@ -21,31 +23,31 @@ int main()
 			myStack.push(screenOutput, stoi(choice), '+');  //Pushes the previous value, the modifying value, and the modifying operator onto the stack
 			screenOutput = screenOutput + stoi(choice);  //Changes the value on the screen and performs the arithmetic
 		}
-		if (choice.at(0) == '-')
+		else if (choice.at(0) == '-')
 		{
 			choice.erase(0, 1);  //Removes the forward operator so only a number is left
 			myStack.push(screenOutput, stoi(choice), '-');  //Pushes the previous value, the modifying value, and the modifying operator onto the stack
 			screenOutput = screenOutput - stoi(choice);  //Changes the value on the screen and performs the arithmetic
 		}
-		if (choice.at(0) == '*')
+		else if (choice.at(0) == '*')
 		{
 			choice.erase(0, 1);  //Removes the forward operator so only a number is left
 			myStack.push(screenOutput, stoi(choice), '*');  //Pushes the previous value, the modifying value, and the modifying operator onto the stack
 			screenOutput = screenOutput * stoi(choice);  //Changes the value on the screen and performs the arithmetic
 		}
-		if (choice.at(0) == '/')
+		else if (choice.at(0) == '/')
 		{
 			choice.erase(0, 1);  //Removes the forward operator so only a number is left
 			myStack.push(screenOutput, stoi(choice), '/');  //Pushes the previous value, the modifying value, and the modifying operator onto the stack
 			screenOutput = screenOutput / stoi(choice);  //Changes the value on the screen and performs the arithmetic
 		}
-		if (choice.at(0) == '%')
+		else if (choice.at(0) == '%')
 		{
 			choice.erase(0, 1);  //Removes the forward operator so only a number is left
 			myStack.push(screenOutput, stoi(choice), '%');  //Pushes the previous value, the modifying value, and the modifying operator onto the stack
 			screenOutput = (screenOutput % stoi(choice));  //Changes the value on the screen and performs the arithmetic
 		}
-		if (choice.at(0) == 'C')
+		else if (choice.at(0) == 'C')
 		{
 			choice = "";
 			screenOutput = 0;
@@ -54,13 +56,23 @@ int main()
 				myStack.pop();
 			}
 		}
-		if (choice.at(0) == 'U')
+		else if (choice.at(0) == 'U') //This is intended to undo the last choice
 		{
-		
+			myStack.pop();  
+			// How do we implement the Stack method and allow the user to redo operations?
+			//Should we make a seperate Stack for undo methods?
 		}
-		if (choice.at(0) == 'R')
+		else if (choice.at(0) == 'R')
 		{
-		
+		 //pops the first value from the RedoStack and pushes it onto the original stack?
+		}
+		else if (choice.at(0) == 'Q')
+		{
+			break;
+		}
+		else
+		{
+			cout << "That command was not recognized";
 		}
 		cout << "Enter a command or an Arithmatic operation followed by a number" << "\n";
 		cout << "Enter + , - , * , / , %   Followed by a number to execute that operation" << "\n";
