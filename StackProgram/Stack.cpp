8c4f2fae9cdd7e69db54f5df3701f3ca3e15d7be
Jeff Schmidt <jeffbs@gmail.com>
2018-02-13 20:59:50 -0600

    Add files via upload
    Implementation of stack.h

--- StackProgram/Stack.cpp
+++ StackProgram/Stack.cpp
@@ -1,53 +1,32 @@
-/* 
- * File:   Stack.cpp
- * Author: jmbind1@ilstu.edu
- * 
- * Created on February 7, 2018, 7:08 PM
- */
+/* stack.cpp, implementation of stack.h */
 
 #include "Stack.h"
+#include <iostream>
 
-Stack::Stack() {
-    top = 0;
-}
-
-void Stack::push(int x, int y, char command)
-{
-    node* temp = new node;
-    temp->x = x;
-    temp->y = y;
-    temp->command = command;
-    
-    if(top == 0)
-    {
-        temp->next = 0;
-    }
-    else
-    {	
-        temp->next = top;
-    }
-    
-    top = temp;
-    delete temp;
+void Stack::push(int x, int y, char command){
+  node *newNode = new node();
+  newNode->x = x;
+  newNode->y = y;
+  newNode->command = command;
+  newNode->next = top;
+  top = newNode;
 }
 
 void Stack::pop(){
-    if(top == 0)
-        return;
-    else{
-        top = top->next;
-    }
+  node *temp = top;
+  if(top==NULL) {
+    std::cout<<"Empty stack.\n";
+    return;
+  }  
+  top = top->next;
+  delete(temp);  //free memory allocated to memory in list
 }
 
-void Stack::display(){
-    
-}
-
-node Stack::readTop(){
-    return top;
+void Stack::display(){    
 }
 
 bool Stack::isEmpty(){
-    return (top==0);
+    if(top==NULL)
+      return true;
+    return false;    
 }
-
