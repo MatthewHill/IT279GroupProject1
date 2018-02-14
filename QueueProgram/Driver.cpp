c6f0654f506c56f9e882648b0759289b97f5da0b
comrad121 <34254430+comrad121@users.noreply.github.com>
2018-02-13 19:25:15 -0600

    Slightly more informative driver

--- QueueProgram/Driver.cpp
+++ QueueProgram/Driver.cpp
@@ -5,10 +5,12 @@
 using namespace std;
 int main() //Not sure if we had an idea for a driver class so I started this.
 {
+	int MaxCustomers; //Most customers in line
+	int MaxWait; //Max wait time for all customers
 	cout << "Enter a value X for maximum arrival and service delay"
 	cin >> input;
-	X = input;
-
+	Clock myClock(0,input); //initialize clock to 0 and a max value of user choosing
+	Clock.generateArrival(); //generate the arrival time for the first Customer
 	cout << "The maximum number of cutomers in the queue at one time was: " << MaxCustomers << "\n"; //Note that the number of customers in the queue does not include the one being waited on
 	cout << "The longest wait any one customer experiences was: " << MaxWait << "\n" //wait does not include service time
 }
