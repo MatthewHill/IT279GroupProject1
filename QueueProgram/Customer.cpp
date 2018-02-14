1b3b757505152b078700996f06f18d33c97d92af
comrad121 <34254430+comrad121@users.noreply.github.com>
2018-02-13 19:23:25 -0600

    Made get method for CustomerNumber

--- QueueProgram/Customer.cpp
+++ QueueProgram/Customer.cpp
@@ -11,14 +11,14 @@ Customer::Customer() {
 	Customer(0, 0, 0 ,0);
 }
 
-Customer::Customer(int arrivalMinute, int departureMinute, int serviceTime) {
+Customer::Customer(int arrivalMinute, int departureMinute, int serviceTime, int customerNumber) {
 	this::arrivalMinute = arrivalMinute;
 	this::departureMinute = departureMinute;
 	this::serviceTime = serviceTime;
 	this::customerNumber = customerNumber;
 }
-
 int Customer::getArrivalMinute() { return arrivalMinute; }
 int Customer::getDepartureMinute() { return departureMinute; }
 int Customer::getServiceTime() { return serviceTime; }
+int Customer::getCustomerNumber() { return customerNumber; }
 
